61175728b36a205142a88be25fe601d12dd73bb4
abhiram1117 <abhiram1117@gmail.com>
2023-11-23 00:54:42 +0530

    Enums

---
+++ Enums/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    enum DaysOfWeek
+    {
+        Sunday,    
+        Monday,    
+        Tuesday,   
+        Wednesday, 
+        Thursday,  
+        Friday,    
+        Saturday   
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Enter a day of the week:");
+        string userInput = Console.ReadLine();
+
+        if (Enum.TryParse<DaysOfWeek>(userInput, ignoreCase: true, out DaysOfWeek userDay))
+        {
+            switch (userDay)
+            {
+                case DaysOfWeek.Sunday:
+                    Console.WriteLine("It's a holiday!");
+                    break;
+                case DaysOfWeek.Monday:
+                case DaysOfWeek.Tuesday:
+                case DaysOfWeek.Wednesday:
+                case DaysOfWeek.Thursday:
+                    Console.WriteLine("It's a working day.");
+                    break;
+                case DaysOfWeek.Friday:
+                    Console.WriteLine("Weekend is almost here!. It's a working day");
+                    break;
+                case DaysOfWeek.Saturday:
+                    Console.WriteLine("It's weekend!");
+                    break;
+                default:
+                    Console.WriteLine("Invalid day entered.");
+                    break;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid day of the week.");
+        }
+    }
+}
